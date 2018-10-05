8cfd52df7caaf8e38963a8dd492342c1926ce89c
Victoria Hobbs <vlhobbs@uark.edu>
2018-10-05 13:37:10 -0500

    Web update and CSS template update

--- frontend/template.php
+++ frontend/template.php
@@ -8,69 +8,76 @@ session_start();
 
 ?>
 <html>
-<head>
+    <head>
 <!--
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 -->
 
-<link rel="stylesheet" type="text/css" href="scstyle_01.css">
+        <link rel="stylesheet" type="text/css" href="scstyle_01.css">
 
-<title>Capstone Template (Should-Cost)</title>
-</head>
-<body>
-
-<nav>
-  <a href="#">User Name</a>
-  <button>
-    <span></span>
-  </button>
-  <div>
-    <ul>
-	
+        <title>Capstone Template (Should-Cost)</title>
+    </head>
+    <body>
+        <div class="sidebar">
+            <a href="#">User Name</a>
+          
+            <br>
 <!-- Dropdown Products -->
-      <li>
-        <a>Products</a>
-        <div>
-          <a href="searchproduct.php">Product Search</a>
-          <a href="addproduct.php">Add Product</a>
-        </div>
-      </li>
+<!-- Do I need the list? Let's try w/o it -->
+                
+            <div class="dropdown">
+                <button class="dropbutton">Products</button>
+                <div class="dropcontent">
+                    <a href="searchproduct.php">Product Search</a>
+                    <a href="addproduct.php">Add Product</a>
+                </div>
+            </div>
+                    
 
 <!-- Dropdown Commodities -->
-      <li>
-        <a href="#">
-          Commodities
-        </a>
-        <div>
-          <a href="searchcommodity.php">Commodity Search</a>
-          <a href="addcommodity.php">Add Commodity</a>
-        </div>
-      </li>
+            <div class="dropdown">
+               <button class="dropbutton">Commodities</button>
+               <div class="dropcontent">
+                   <a href="searchcommodity.php">Commodity Search</a>
+                   <a href="addcommodity.php">Add Commodity</a> 
+               </div>
+           </div>
+                    
 
 <!-- Hyperlinks -->
-      <a href="recentsearches.php">Recent Searches</a>
-     <a href="logout.php">Logout</a>
-    </ul>
+          <div class="dropdown">
+              <a href="recentsearches.php"><button class="dropbutton">Recent Searches</button></a>
+          </div>
+          <div class="dropdown">
+              <a href="logout.php"><button class="dropbutton">Logout</button></a>
+          </div>
+              
+      </div>
+      
 
-  </div>
-</nav>
+      <br></br>
 
-<br></br>
-
-<div>
-<div><h1>Template</h1></div>
-
-<br> <?php
-
+      <div class="main">
+            <h1>Template</h1>
+    
+            <br>Test text goes here for seeing how the page looks with it.
+            <br>Need to make sure that it is displayed properly.
+            <form>
+                <label for="test1">Test Field 1</label>
+                <input type="text" placeholder="Input 1">
+                <br>
+                <label for "test2">Test Field 2</label>
+                <input type="text" placeholder="Input 2">
+            </form>
+            <br><button>Test button</button>
+ <?php
+//Code
 ?>
 
+        </div>
 
 
-
-</div>
-
-
-</body>
+    </body> 
 </html>
 
 
