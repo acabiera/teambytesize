525d2b39e5a45776c3b67e6f9ac5df4017210578
Victoria Hobbs <vlhobbs@uark.edu>
2018-10-06 00:32:52 -0500

    updated login.php to new stylesheet

--- frontend/login.php
+++ frontend/login.php
@@ -7,89 +7,96 @@ if (isset($_SESSION['valid'])){
 ?><!DOCTYPE HTML>
 
 <html>
-<body>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<title>Login</title>
-<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <head>
+        <link rel="stylesheet" type="text/css" href="scstyle_01.css">
+        <title>Login</title>
+    </head>
+    <body>
+        <div class="sidebar">
+            
 
 
 <?php
 
-require 'vendor/autoload.php';
-
-use scservice\SCConnect as Connect;
-echo 'Please Log In';
+    require 'vendor/autoload.php';
+    use scservice\SCConnect as Connect;
+    echo 'Please Log In';
 ?>
 
-<body style='background-color:silver'>
+<!-- This is the sidebar, which I don't need here
+            <br>
+ Dropdown Products 
+            <div class="dropdown">
+                <button class="dropbutton">Products</button>
+                <div class="dropcontent">
+                    <a href="searchproduct.php">Product Search</a>
+                    <a href="addproduct.php">Add Product</a>
+                </div>
+            </div>
 
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-<span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-<div class="navbar-nav">
+ Dropdown Commodities
 
-</div>
-</div>
-</nav>
-<br></br>
-<center>
-<div class="card bg-primary" style="width: 50rem;">
-<div class="h1 card-title">Should-Cost Login</div>
+            <div class="dropdown">
+                <button class="dropbutton">Commodities</button>
+                <div class="dropcontent">
+                    <a href="searchcommodity.php">Commodity Search</a>
+                    <a href="addcommodity.php">Add Commodity</a>
+                </div>
+            </div>
 
+Hyperlinks
 
-
-
-<div class = "card bg-primary" style="width:20rem;margin: 0 auto;">
-<!--
-<form action='login.php' method='post'>
+            <div class="dropdown">
+                <a href="recentsearches.php"><button class="dropbutton">Recent Searches</button></a>
+            </div>
+            <div class="dropdown">
+                <a href="logout.php"><button class="dropbutton">Logout</button></a>
+            </div>
 -->
-<form action='authenticate.php' method='post'>
-	<br>
-	<div style="width: 85%;">
-		<label style="float: left" for="username"><b>Username</b></label>
-		<input style="float: right" type="text" placeholder="Your username" name="username" required>
-	</div>
-	<div style="width: 85%; clear: both;">
-		<label style="float: left" for="password"><b>Password</b></label>
-		<input style="float: right" type="password" placeholder="Your password" name="password" required>
-	</div>
+        </div>
 
-	<?php
-	if (isset($_SESSION['badPass'])){
-		echo '<script> window.alert("Your password was incorrect! Please retry.")</script>';
-	   unset($_SESSION['badPass']);
-	}
+        <br>
 
-	if (isset($_SESSION['userExists'])){
-		echo '<script> window.alert("This username always exists. Either go back and create with a new username, or log in!")</script>';
-	   unset($_SESSION['userExists']);
-	}
-	if (isset($_SESSION['newUserMade'])){
-		echo '<script> window.alert("User account created successfully!")</script>';
-		unset($_SESSION['newUserMade']);
-	}
-        if (isset($_SESSION['error'])){
-                echo '<script> window.alert("connection error") </script>';
-                unset($_SESSION['error']);
-        }
+        <div class="main">
+            <h1>Should-Cost Login</h1>
+            <div class="noborder"> 
+                <form action='authenticate.php' method='post'>
+                    <br>
+                    <label for="username"><b>Username</b></label>
+                    <input type="text" placeholder="Your username" name="username" required>
+                    <br>
+                    <label for="password"><b>Password</b></label>
+                    <input type="password" placeholder="Your password" name="password" required>
+                
 
-	?>
-	<br style="clear: both">
-	<br>
-	<div style="width: 85%; margin-bottom: 0px;">
-		<button style="clear: both;" class="border btn btn-primary border-dark text-dark" type="submit"> Log in</button>
-	</div>
-	<a class="nav-item nav-link text-dark" href="createuser.php">Create User</a>
-</form>
-</div>
+<?php
+    if (isset($_SESSION['badPass'])){
+        echo '<script> window.alert("Your password was incorrect! Please retry.")</script>';
+        unset($_SESSION['badPass']);
+    }
 
-<br>
+    if (isset($_SESSION['userExists'])){
+        echo '<script> window.alert("This username always exists. Either go back and create with a new username, or log in!")</script>';
+        unset($_SESSION['userExists']);
+    }
+    if (isset($_SESSION['newUserMade'])){
+        echo '<script> window.alert("User account created successfully!")</script>';
+        unset($_SESSION['newUserMade']);
+    }
+    if (isset($_SESSION['error'])){
+        echo '<script> window.alert("connection error") </script>';
+        unset($_SESSION['error']);
+    }
 
-</div>
-</center>
-
-</body>
+?>
+                    <br>
+                    <button type="submit"> Log in</button>
+                </form>
+                <div class="noborder">
+                    <br> 
+	            <a class="nav-item nav-link text-dark" href="createuser.php">Create User</a>
+                </div>
+            </div>
+    </body>
 </html>
-
 
